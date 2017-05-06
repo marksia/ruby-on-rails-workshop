df33e5822ce5569e9e4fbeb1bd9e167b9d860404
Mark Sia Hansheng <mark.siah@oceannexus.my>
2017-05-06 15:56:14 +0800

    Add 'Guess' resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'guesses/create'
+
   root to: 'games#index'
 
-  resources :games
+  resources :games do
+    resources :guesses, only: :create
+    # post '/guesses', to: "guesses#create"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506055320) do
+ActiveRecord::Schema.define(version: 20170506062843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 20170506055320) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "guesses", force: :cascade do |t|
+    t.integer "number"
+    t.bigint "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_guesses_on_game_id"
+  end
+
+  add_foreign_key "guesses", "games"
 end
